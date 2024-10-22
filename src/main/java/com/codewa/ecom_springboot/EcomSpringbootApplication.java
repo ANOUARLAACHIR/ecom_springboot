c49638ec88af6f23473cdc7d14615662940ab186
Anouar LAACHIR <become.engineer88@gmail.com>
2024-10-22 22:41:41 +0100

    first commit

---
+++ src/main/java/com/codewa/ecom_springboot/EcomSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.codewa.ecom_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomSpringbootApplication.class, args);
+    }
+
+}
